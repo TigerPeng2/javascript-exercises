51dc887a862e783db6273ea61c07aff9890cd289
Tiger Peng <nytigeryxp@gmail.com>
2022-08-08 20:47:17 -0400

    Complete all Fundamentals 4 exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let out = '';
 
+    for (let i = text.length - 1; i >= 0; i--) {
+        out += text.substring(i,i + 1);
+    }
+
+    return out;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+    for (let i = 0; i < toRemove.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (toRemove.includes(arr[j])) {
+                arr.splice(j, 1);
+                j--;
+            }
+        }
+    }
 
+    return arr;
 };
 
+removeFromArray([1, 2, 3, 2, 1, 2, 1, 3], 2, 1);
 // Do not edit below this line
 module.exports = removeFromArray;
